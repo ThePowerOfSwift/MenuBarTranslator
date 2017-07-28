c3ed569aac121533829209236bdbad3e52eb1a0a
Unknown <artembobrov3059@gmail.com>
2017-07-28 21:52:14 +0200

    Main code

---
+++ MenuBarTranslator/AppDelegate.swift
@@ -0,0 +1,54 @@
+//
+//  AppDelegate.swift
+//  MenuBarTranslator
+//
+//  Created by Artem Bobrov on 28.07.17.
+//  Copyright © 2017 Artem Bobrov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    let statusItem = NSStatusBar.system().statusItem(withLength: -2)
+    let popover = NSPopover()
+    var eventMonitor: EventMonitor?
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        if let button = statusItem.button {
+            button.image = NSImage(named: "StatusBarButtonImage")
+            button.action = #selector(togglePopover(_:))
+        }
+        popover.contentViewController = TranslateViewController(nibName: "TranslateViewController", bundle: nil)
+        
+        eventMonitor = EventMonitor(mask: [.leftMouseDown, .rightMouseDown]) { [unowned self] event in
+            if self.popover.isShown {
+                self.closePopover(event)
+            }
+        }
+        eventMonitor?.start()
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+    }
+    
+    func showPopover(_ sender: AnyObject?) {
+        if let button = statusItem.button {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+        }
+    }
+    
+    func closePopover(_ sender: AnyObject?) {
+        popover.performClose(sender)
+    }
+    
+    func togglePopover(_ sender: AnyObject) {
+        if popover.isShown {
+            closePopover(sender)
+        } else {
+            showPopover(sender)
+        }
+    }
+}
+
---
+++ MenuBarTranslator/TranslateViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TranslateViewController.swift
+//  MenuBarTranslator
+//
+//  Created by Artem Bobrov on 28.07.17.
+//  Copyright © 2017 Artem Bobrov. All rights reserved.
+//
+
+import Cocoa
+
+class TranslateViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear() {
+        super.viewDidAppear()
+    }
+    
+}
